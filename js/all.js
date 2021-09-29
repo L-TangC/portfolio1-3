9bb269a4bf01002439e466d46e9cb4be24d5cd72
L-TangC <87227806+L-TangC@users.noreply.github.com>
2021-09-29 18:28:31 +0800

    add moodapp scroll on js
    $(window).scroll(function() {
      var scrolledY = $(window).scrollTop();
      $('.moodapp').css('background-position', 'left ' + ((scrolledY)) + 'px');
    });

--- js/all.js
+++ js/all.js
@@ -12,3 +12,7 @@ $(window).scroll(function() {
   var scrolledY = $(window).scrollTop();
   $('.portfolio-work').css('background-position', 'left ' + ((scrolledY)) + 'px');
 });
+$(window).scroll(function() {
+  var scrolledY = $(window).scrollTop();
+  $('.moodapp').css('background-position', 'left ' + ((scrolledY)) + 'px');
+});
